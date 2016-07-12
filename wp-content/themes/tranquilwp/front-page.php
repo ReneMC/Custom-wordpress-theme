99c454bef6bf6ccf8732cd7f70a1edb87beeba45
renem <rene.mejiac@gmail.com>
2016-07-12 10:27:42 -0700

    + WP get_template_part function for loading modules

--- wp-content/themes/tranquilwp/front-page.php
+++ wp-content/themes/tranquilwp/front-page.php
@@ -10,28 +10,7 @@
                </div><!-- /col -->
            </div><!-- /row -->
            
-           <div class="row facilities">
-               <div class="col-sm-3">
-                   <a href="">
-                       <img src="<?php echo esc_url(get_template_directory_uri() );?>/images/makeup.png" alt="Hair & Make up">Hair &amp; Make Up
-                   </a>
-               </div><!-- /col -->
-               <div class="col-sm-3">
-                   <a href="">
-                       <img src="<?php echo esc_url(get_template_directory_uri() );?>/images/candles.png" alt="Spa Facilities">Spa Facilities
-                   </a>
-               </div><!-- /col -->
-               <div class="col-sm-3">
-                   <a href="">
-                       <img src="<?php echo esc_url(get_template_directory_uri() );?>/images/lake.png" alt="Lakeside Retreat">Lakeside Retreat
-                   </a>
-               </div><!-- /col -->
-               <div class="col-sm-3">
-                   <a href="">
-                       <img src="<?php echo esc_url(get_template_directory_uri() );?>/images/pool.png" alt="All Our Facilities">All Our Facilities
-                   </a>
-               </div><!-- /col -->
-           </div><!-- /row -->
+            <?php get_template_part('content-facilities'); ?>
        </div><!-- /container -->
     </section>
     
